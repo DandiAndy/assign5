5a03b5721efef29286c55f1b4cfb014ba44038bf
Muhammad Hamza Awan <awan@bluenose.cs.dal.ca>
2017-03-22 15:42:42 -0300

    added additional feautures

--- nb.py
+++ nb.py
@@ -1,6 +1,8 @@
+from __future__ import division
 import itertools
 import io
 import glob
+from math import log
 
 def CleanData(f, filename):
     print("...clean data...")
@@ -32,8 +34,57 @@ def BuildClassifier(tuples, class_vals,  attr_vals, class_index, attributes):
     #print("Class Values:\n{}\nAttribute Values:\n{}".format(class_vals, attr_vals))
     
     
-def Classify():
+def Classify(tuples, class_attr, attr_vals, attributes, class_index):
     print("...classify...")
+    for a in attributes:
+        print("{} ".format(a)),
+    print("Classification")
+    count = sum(class_attr.values())
+    m = 0.2
+    print(attr_vals)
+    #create ditionary for p values to avoid lack of label
+    temp = attr_vals.keys()
+    p_list = {}
+    check = {}
+    for attr in temp:
+            if p_list.has_key(attr.split()[1]):
+                if attr.split()[2] not in p_list.get(attr.split()[1]):
+                    p_list.update({attr.split()[1] : p_list.get(attr.split()[1]) + [attr.split()[2]]})
+            else:
+                p_list.update({attr.split()[1] : list([attr.split()[2]])})
+    #print(p_list)
+
+    #run through tuples and and attribute to create the scores for each of the classes attribute values to determine the prediction
+    for t in tuples:
+        key_list = class_attr.keys()
+        class_val = ""
+        b = True
+        key_val = 0.0
+        for key in key_list:
+            nb_value = class_attr.get(key)/count
+            #print("nb: {}, class count: {}".format(nb_value, class_attr.get(key)))
+            i = 0
+            for a in t:
+                attr_key_list = attr_vals.keys()
+                for k in attr_key_list:
+                    #print("{} {} {}".format(key, attributes[i], a))
+                    if key in k.split() and attributes[i] in k.split() and a in k.split():
+                        attr_count = (attr_vals.get(k)+ m*len(p_list.get(attributes[i])))/(class_attr.get(key) + m)
+                        #attr_count = (attr_vals.get(k)/count)
+                        nb_value *= attr_count
+                        #print(nb_value)
+                if b:
+                    print("{}\t\t".format(a)),
+                i += 1
+            #print("{} : {}".format(key, nb_value))
+            if key_val < nb_value:
+                key_val = float(nb_value)
+                class_val = str(key)
+            #print("{} {}".format(key_val, nb_value))
+            b = False
+        print("{}".format(class_val))
+
+
 
 if __name__ == "__main__":
     print("...main...")
@@ -99,12 +150,12 @@ if __name__ == "__main__":
     for line in f:
         if len(line.split()) > 0:
             test_tuples.append(line.split())
-    print(test_tuples)
+    #print(test_tuples)
     f.close()
 
     
     #classify the given test data and print to Results.txt
-    Classify()
+    Classify(test_tuples, class_attr_vals, attr_vals, attributes, class_index)
     
     
     
